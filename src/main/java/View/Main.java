7685113d5529db59708998f6d3412a70893697bd
Saul Consuegra <sconsuegra@gmail.com>
2025-01-06 20:49:40 -0500

    CHANGE: Se elimina TorneoMVC.java y se remlaza por Main.java

---
+++ src/main/java/View/Main.java
@@ -0,0 +1,12 @@
+package View;
+
+import View.Presentacion;
+
+public class Main {
+    public static void main(String[] args) {
+        Presentacion view = new Presentacion();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+
+    }
+}
